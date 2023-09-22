93e2d46524a1de2a86cb088d11f4c74b38aab4f2
Jake Jasmin <roastbeefer000@gmail.com>
2023-09-21 19:51:51 -0600

    colors and styling

--- packages/web/src/routes/+page.svelte
+++ packages/web/src/routes/+page.svelte
@@ -1,24 +1,38 @@
 <script>
-  import logo from "../assets/svelte.png";
-  import Alert from "../lib/Alert.svelte";
-  import Counter from "../lib/Counter.svelte";
+    import SunIcon from "virtual:icons/ph/sun";
+    import MoonIcon from "virtual:icons/ph/moon";
+    let theme = "mocha"
+
+    function toggle_theme() {
+        if (theme == "mocha") {
+            theme = "latte";
+        }
+        else {
+            theme = "mocha";
+        }
+    }
 </script>
 
-<main class="text-center p-1 my-0 mx-auto">
-  <img class="mx-auto h-64 w-64" src={logo} alt="Svelte Logo" />
-  <h1 class="max-w-none min-w-[480px] max-w-[14rem] text-[#ff3e00] uppercase text-6xl font-thin leading-12 my-2 mx-auto">Hello Vite+Rust!</h1>
-
-  <div>
-    <Counter />
-  </div>
-  <div class="mt-3">
-    <Alert />
-  </div>
-
-  <p class="max-w-md my-1 mx-auto leading-5">
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a>, powered by Vite!
-  </p>
-</main>
+<html class="{theme}">
+    <body class="bg-base h-screen w-screen text-center transition-colors">
+        <div class="fixed flex p-4 right-0 top-0">
+            <MoonIcon class={theme === "mocha" ? "text-lavender text-xl mr-1" : "text-transparent text-xl mr-1"} />
+            <label for="toggle" class="relative flex cursor-pointer">
+                <input type="checkbox" id="toggle" class="sr-only peer" />
+                <div on:click={toggle_theme}  class="h-6 bg-lavender border-2 border-lavender rounded-full w-11 after:absolute after:top-0.5 after:left-0.5 after:bg-white after:border after:border-lavender after:h-5 after:w-5 after:shadow-sm after:rounded-full peer-checked:after:translate-x-full peer-checked:after:border-white peer-checked:bg-yellow peer-checked:border-yellow after:transition-all after:duration-300"></div>
+            </label>
+            <SunIcon class={theme === "latte" ? "text-yellow text-xl mr-1" : "text-transparent text-xl mr-1"} />
+        </div>
+        <div class="pt-10 item-center justify-center">
+            <h1 class="text-blue uppercase text-6xl font-bold leading-12 my-2 mx-auto">TODO</h1>
+            <div class="max-w-[85%] sm:max-w-2xl m-2 mx-auto"> 
+                <div>
+                    <input type="text" placeholder="What needs to be done?" class="w-full h-full p-4 bg-surface0 focus:border-4 focus:ring-blue focus:border-blue outline-none text-text transition-all">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
 
 <style>
   :root {
--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -2,7 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
-	plugins: [wasm(), topLevelAwait(), sveltekit()]
+    plugins: [wasm(), topLevelAwait(), sveltekit(), Icons({ compiler: 'svelte', })]
 });
